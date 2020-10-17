5ebd76ded9f20bc9bc158b99ed4c5323883b718f
Algrtm <70532776+Algrtm@users.noreply.github.com>
2020-10-17 14:07:31 -0400

    initial

---
+++ src/UI/Window.java
@@ -0,0 +1,12 @@
+package UI;
+
+import javax.swing.*;
+
+public class Window {
+    static JFrame frame;
+
+    public static void startWindow(int width, int height) {
+        frame = new JFrame("MotiveApp");
+
+    }
+}
