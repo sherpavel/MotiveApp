69b8e5ae4946d0c3c363af5e6406056793e10a91
vnarendar <vnarendar@ucmerced.edu>
2020-10-17 12:40:43 -0700

    updated motive

--- src/MotiveApp.java
+++ src/MotiveApp.java
@@ -1,20 +1,39 @@
 //import ui.Window;
 import java.util.*;
+import java.io.*;
 
 public class MotiveApp {
+
 
   public static void main(String[] args) {
     //Window.startWindow(800, 800);
 
-
     // We can take this value as an input
     int numTasks = 10;
     boolean tasksLeft = true;
+
+    int pointGoal = 0;
     int totalPoints = 0;
 
     List<Task> taskList = new ArrayList<Task>(numTasks);
     // Read tasks from the .dat file and create and add the tasks to the taskList
-
+    Scanner sc = null;
+    try {
+      File file = new File("./data/tasks.dat");
+      sc = new Scanner(file);
+    }
+    catch (FileNotFoundException e){
+      System.out.println("File not found");
+    }
+    // Read the data file
+    while(sc.hasNextLine()){
+      String line = sc.nextLine();
+      String[] lineSplit = line.split(",");
+      String name = lineSplit[0];
+      String taskSize = lineSplit[1];
+      taskList.add(new Task(name, taskSize));
+    }
+    sc.close();
 
     while (tasksLeft) {
       // Output a task for the person to do at specific times
