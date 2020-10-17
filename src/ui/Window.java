4c9b8489e4bc096a8a2c0f874c609fc310ccb0c9
Algrtm <70532776+Algrtm@users.noreply.github.com>
2020-10-17 15:09:48 -0400

    data + ui

--- src/ui/Window.java
+++ src/ui/Window.java
@@ -1,14 +1,24 @@
 package ui;
 
+import data.Settings;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class Window {
     static JFrame frame;
 
-    public static void startWindow(int width, int height) {
-        frame = new JFrame("MotiveApp");
-        frame.setPreferredSize(new Dimension(width, height));
+    public static void startWindow() {
+        Settings settings = Settings.getInstance();
+        int width = Integer.parseInt(settings.getValue("windowWidth"));
+        int height = Integer.parseInt(settings.getValue("windowHeight"));
 
+        frame = new JFrame("MotiveApp");
+        frame.setSize(width, height);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        frame.setVisible(true);
     }
 }
