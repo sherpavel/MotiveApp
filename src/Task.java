852a5a559a142125363533a9903dcfadeec43aab
vnarendar <vnarendar@ucmerced.edu>
2020-10-17 12:29:35 -0700

    Update Task Objects with task.dat
    Going to read from a simple file using delimters
    
    Co-Authored-By: Robert Gan <rdgan@ad.unc.edu>

--- src/Task.java
+++ src/Task.java
@@ -1,18 +1,11 @@
-import static javax.swing.text.html.HTML.Tag.SMALL;
-
 public class Task {
 
-  private enum taskType {SMALL, MEDIUM, LARGE}
-
-  ;
   private String name;
-  private int timeDuration;
-  private taskType taskSize;
+  private String  taskSize;
 
 
-  public Task(String name, int timeDuration, taskType taskSize) {
+  public Task(String name, String taskSize) {
     this.name = name;
-    this.timeDuration = timeDuration;
     this.taskSize = taskSize;
   }
 
@@ -20,19 +13,13 @@ public class Task {
     return this.name;
   }
 
-  public int getTimeDuration() {
-    return this.timeDuration;
-  }
 //User asses how large/significant the task is assigned Small, Medium, and Large
   public int getPointValue() {
-    switch (taskSize) {
-      case SMALL:
-        return 5;
-      case MEDIUM:
-        return 10;
-      case LARGE:
-        return 20;
-    }
-    return 0;
+    return switch (taskSize) {
+      case "SMALL" -> 5;
+      case "MEDIUM" -> 10;
+      case "LARGE" -> 20;
+      default -> 0;
+    };
   }
 }
