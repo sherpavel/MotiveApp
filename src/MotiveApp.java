6d7211237b93cd8847b74c85c8c4fd488f5fe33d
vnarendar <vnarendar@ucmerced.edu>
2020-10-17 11:36:28 -0700

    Created Task object and beginning of UI
    started to build fuctionality of task assignemnt for points
    
    Co-Authored-By: Robert Gan <rdgan@ad.unc.edu>

--- src/MotiveApp.java
+++ src/MotiveApp.java
@@ -1,2 +1,29 @@
+//import ui.Window;
+import java.util.*;
+
 public class MotiveApp {
+
+  public static void main(String[] args) {
+    //Window.startWindow(800, 800);
+
+
+    // We can take this value as an input
+    int numTasks = 10;
+    boolean tasksLeft = true;
+    int totalPoints = 0;
+
+    List<Task> taskList = new ArrayList<Task>(numTasks);
+    // Read tasks from the .dat file and create and add the tasks to the taskList
+
+
+    while (tasksLeft) {
+      // Output a task for the person to do at specific times
+
+
+      // If they finished the task then remove it from the list and add points
+//      totalPoints += taskList.get( ).getPointValue();
+      tasksLeft = false;
+    }
+
+  }
 }
--- src/Task.java
+++ src/Task.java
@@ -1,13 +1,19 @@
+import static javax.swing.text.html.HTML.Tag.SMALL;
+
 public class Task {
 
+  private enum taskType {SMALL, MEDIUM, LARGE}
+
+  ;
   private String name;
   private int timeDuration;
-  private int pointValue;
+  private taskType taskSize;
 
-  public Task(String name, int timeDuration, int pointValue) {
+
+  public Task(String name, int timeDuration, taskType taskSize) {
     this.name = name;
     this.timeDuration = timeDuration;
-    this.pointValue = pointValue;
+    this.taskSize = taskSize;
   }
 
   public String getName() {
@@ -17,8 +23,16 @@ public class Task {
   public int getTimeDuration() {
     return this.timeDuration;
   }
-
+//User asses how large/significant the task is assigned Small, Medium, and Large
   public int getPointValue() {
-    return this.pointValue;
+    switch (taskSize) {
+      case SMALL:
+        return 5;
+      case MEDIUM:
+        return 10;
+      case LARGE:
+        return 20;
+    }
+    return 0;
   }
 }
